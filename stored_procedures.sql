355537a5ef75241644ae144657657b73c97dedd1
luxfinis <luxfinis@ruggedinbox.com>
2015-04-02 16:24:29 +0200

    corrected 'sucheBuecherNachISBN', 'sucheBuecherNachFachgebietId'

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -5,30 +5,35 @@
 
 USE[Bibliothek]
 GO
-
-CREATE PROCEDURE sp_SucheBuecherNachISBN (@pattern nvarchar)
+CREATE PROCEDURE sp_SucheBuecherNachISBN (@pattern bigint)
 AS
 SELECT * 
 FROM Buecher
 WHERE p_ISBN = @pattern
-
 GO
+EXEC sp_SucheBuecherNachISBN 3499624249
 
+
+USE[Bibliothek]
+GO
 CREATE PROCEDURE sp_SucheBuecherNachTitel (@pattern nvarchar)
 AS
 SELECT *
 FROM Buecher
 WHERE titel like '%'+@pattern+'%'
-
 GO
+EXEC sp_SucheBuecherNachTitel 'Dinge geregelt kriegen'
 
-CREATE PROCEDURE sp_SucheBuecherNachFachgebietId (@pattern nvarchar)
+
+USE[Bibliothek]
+GO
+CREATE PROCEDURE sp_SucheBuecherNachFachgebietId (@pattern int)
 AS
 SELECT * 
 FROM Buecher
 WHERE f_fachgebiet_id = @pattern
-
 GO
+EXEC sp_SucheBuecherNachFachgebietId 1
 
 
 -- rückgabe der bücher ist ohne ausweis möglich
