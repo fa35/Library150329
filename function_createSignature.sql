84a92575b28a281379cb001bcfd3d0c58f2ec431
luxfinis <luxfinis@ruggedinbox.com>
2015-04-03 16:58:35 +0200

    added procedure added new function for creating a signature

---
+++ function_createSignature.sql
@@ -0,0 +1,34 @@
+USE[Bibliothek]
+GO
+CREATE FUNCTION CreateSignature (@fachgebietId int, @isbn bigint)
+RETURNS varchar(10)
+AS
+BEGIN
+DECLARE @kuezel varchar(3) = (select SUBSTRING(kuerzel, 1, 3) from Fachgebiete where p_fachgebiet_id = @fachgebietId)
+DECLARE @name varchar(1) = (select SUBSTRING(name, 1,1) from Autoren where p_autor_id = (select pf_autor_id from Buecher_Autoren where pf_isbn = @isbn))
+DECLARE @anzahl int = (select count(*) from Exemplare where f_ISBN = @isbn);
+DECLARE @nummer varchar(4) = '0001';
+
+IF (@anzahl > 0)
+	BEGIN
+	IF @anzahl <= 9
+		BEGIN
+			SET @nummer = ('000' + @anzahl);
+		END
+	ELSE IF @anzahl >= 10 and @anzahl < 100
+		BEGIN
+			SET @nummer = ('00' + @anzahl); 
+		END
+	ELSE IF @anzahl >= 100 and @anzahl < 1000
+		BEGIN
+			SET @nummer = ('0' + @anzahl);
+		END
+	ELSE
+		BEGIN 
+			SET @nummer = @anzahl;
+		END
+   END
+
+RETURN (@kuezel + @name + @nummer)
+END
+GO
--- stored_procedures.sql
+++ stored_procedures.sql
@@ -222,12 +222,80 @@ EXEC sp_LoescheBuch 2, 3518260430
 
 
 
+-- Funktion CreateSignature zusätzlich wird benötigt
+
+
+USE [Bibliothek]
+GO
+CREATE PROCEDURE sp_LegeExemplarAn @ausweisNr int, @isbn bigint
+AS
+DECLARE @mitarbeiter bit = dbo.GetMitarbeiterBit(@ausweisNr)
+
+IF ( @mitarbeiter = 0)
+	BEGIN 
+		PRINT 'Sie sind nicht berechtigt'
+	END
+ELSE
+	BEGIN
+		DECLARE @fachgebiet int = (select f_fachgebiet_id from Buecher where p_ISBN = @isbn)
+		DECLARE @signature varchar(10) = dbo.CreateSignature(@fachgebiet, @isbn);
+
+		INSERT INTO [dbo].[Exemplare]
+					([p_signatur]
+					,[f_ISBN])
+				VALUES
+					(@signature, @isbn)
+	END
+GO
+
+EXEC sp_LegeExemplarAn 2, 3518260432
+
+
+
+-- Lösche Exemplar
+
+
+USE[Bibliothek]
+GO
+CREATE PROCEDURE sp_LoescheExemplar @ausweisNr int, @signatur varchar(10)
+AS
+DECLARE @mitarbeiter bit = dbo.GetMitarbeiterBit(@ausweisNr)
+
+IF ( @mitarbeiter = 0)
+	BEGIN 
+		PRINT 'Sie sind nicht berechtigt'
+	END
+ELSE
+	BEGIN
+		DECLARE @isbn bigint = (select f_isbn from Exemplare where p_signatur = @signatur);
+
+		DECLARE @anzahl int = (
+			select count(*) 
+			from Ausgeliehene_Exemplare 
+			where pf_signatur = @signatur
+		)
+
+		IF(@anzahl > 0)
+		BEGIN
+			PRINT 'Das Exemplar ist ausgeliehen - kann somit nicht gelöscht werden'
+		END
+		ELSE
+		BEGIN
+			DELETE FROM Exemplare WHERE p_signatur = @signatur
+	END
+END
+GO
+
+EXEC sp_LoescheExemplar 2, 'PROP0001'
+
+-- evtl. cool oder notwendig sowas wie : loesche alle exemplare eines buches
+
 
 
 
 
 mitarbeiter können zusätzlich:
-- exemplare hinzufügen / löschen
+
 - nutzer hinzufügen / löschen
 - ausweis sperren / entsperren / verlängern
 - leihfrist oder gebühr ändern
