8b2238b3d4e77b53b7c018e6380a80b924fc14ba
luxfinis <luxfinis@ruggedinbox.com>
2015-04-04 13:32:10 +0200

    fixed test buch added test for autor

--- tests/sp_test_anlegen_loeschen_buch.sql
+++ tests/sp_test_anlegen_loeschen_buch.sql
@@ -1,49 +1,53 @@
--- buch
-
---DECLARE @ausweisNr int, @isbn bigint, @titel nvarchar(max), @fachgebiet int;
+-- teste ob das anlegen und loeschen eines buches funktioniert
 
 USE [Bibliothek]
 GO
 CREATE PROCEDURE sp_TestAnlegenLoeschenBuch @ausweisNr int, @isbn bigint, @titel nvarchar(max), @fachgebiet int
 AS
-DECLARE @anzBuecherVor int = (select * from Buecher)
-
-PRINT 'Versuche neues Buch anzulegen'
+-- anzahl der buecher vor irgendwelchen aenderungen
+DECLARE @anzBuecherVor int = (select count(*) from Buecher)
+	PRINT 'Versuche neues Buch anzulegen'
+-- ausfuehren der anlege prozedur
 EXEC sp_LegeBuchAn @ausweisNr, @isbn, @titel, @fachgebiet 
-PRINT 'Prozedur sp_LegeBuchAn ausgefuehrt'
-
-DECLARE @anzBuecherNach int = (select * from Buecher)
-
-PRINT CONCAT('Anzahl vor: ', @anzBuecherVor, ' Anzahl nach: ', @anzBuecherNach)
-
-IF(@anzBuecherVor < @anzBuecherNach)
-BEGIN
-	PRINT 'Prozedur sp_LegeBuchAn funktioniert'
-	-- loesche buch
-
-	PRINT 'Versuche Buch zu loeschen'
-	EXEC sp_LoescheBuch @ausweisNr, @isbn
-	PRINT 'Prozedur sp_LoescheBuch ausgefuehrt'
-
-	SET @anzBuecherVor = @anzBuecherNach
-	SET @anzBuecherNach = (select * from Buecher)
-
+	PRINT 'Prozedur sp_LegeBuchAn ausgefuehrt'
+-- anzahl der buecher nachdem ein buch angelegt wurde
+DECLARE @anzBuecherNach int = (select count(*) from Buecher)
 	PRINT CONCAT('Anzahl vor: ', @anzBuecherVor, ' Anzahl nach: ', @anzBuecherNach)
 
-	IF(@anzBuecherVor > @anzBuecherNach)
-	BEGIN
-		PRINT 'Prozedur sp_LoescheBuch funktioniert'
-	END
+-- wenn anzahl der buecher davor kleiner als die anzahl der buecher danach ist
+IF(@anzBuecherVor < @anzBuecherNach)
+BEGIN
+PRINT 'Prozedur sp_LegeBuchAn funktioniert'
+--- suche in der tabelle nach diesem einen datensatz
+	DECLARE @catched int = (select count(*) from Buecher where p_ISBN = @isbn and titel = @titel and f_fachgebiet_id = @fachgebiet)
+PRINT CONCAT('Es gibt genau: ', @catched, ' Eintrag/Eintraege, welche die eingegebenen Daten haben.')
+
+-- loesche buch
+PRINT 'Versuche Buch zu loeschen'
+	EXEC sp_LoescheBuch @ausweisNr, @isbn
+PRINT 'Prozedur sp_LoescheBuch ausgefuehrt'
+-- nachdem ein buch geloescht wurde, müsste die anzahl der buecher wieder so sein wie am anfang
+	SET @anzBuecherNach = (select count(*) from Buecher)
+PRINT CONCAT('Anzahl vor: ', @anzBuecherVor, ' Anzahl nach: ', @anzBuecherNach)
+-- suche in der tabelle dem geloeschten datensatz
+	SET @catched = (select count(*) from Buecher where p_ISBN = @isbn and titel = @titel and f_fachgebiet_id = @fachgebiet)
+PRINT CONCAT('Es gibt genau: ', @catched, ' Eintrag/Eintraege, welche die eingegebenen Daten haben.')
+
+	IF(@anzBuecherVor = @anzBuecherNach)
+		BEGIN
+			PRINT 'Prozedur sp_LoescheBuch funktioniert'
+		END
 	ELSE
-	BEGIN
-		PRINT 'Prozedur sp_LoescheBuch scheint nicht richtig zu funktionieren'
-	END
+		BEGIN
+			PRINT 'Prozedur sp_LoescheBuch scheint nicht richtig zu funktionieren'
+		END
 END
 ELSE
-BEGIN
-	PRINT 'Prozedur sp_LegeBuchAn scheint nicht richtig zu funktionieren'
-END
+	BEGIN
+		PRINT 'Prozedur sp_LegeBuchAn scheint nicht richtig zu funktionieren'
+	END
+
 
 exec sp_TestAnlegenLoeschenBuch 2, 9999999999, 'test buch anlegen', 1
 
-select * from buecher
+
