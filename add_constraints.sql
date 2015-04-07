bd63ac29e92884f0dc27839523935cfd688f88c6
luxfinis <luxfinis@ruggedinbox.com>
2015-04-07 13:32:27 +0200

    added some constraints

---
+++ add_constraints.sql
@@ -0,0 +1,22 @@
+
+-- Oeffnungszeiten: von < bis
+ALTER TABLE Oeffnungszeiten
+ADD CONSTRAINT ct_bis CHECK (von < bis)
+
+-- Leihfrist mindestens 1 Wochen
+ALTER TABLE Bibliotheken
+ADD CONSTRAINT ct_leihwochen CHECK (leihfrist_wochen >= 1)
+
+-- Gebuehren >= 0
+ALTER TABLE Bibliotheken
+ADD CONSTRAINT ct_jahresgebuehr CHECK (gebuehren_jahr >= 0 
+										AND ((gebuehren_jahr - gebuehren_jahr) = 0))
+
+ALTER TABLE Bibliotheken
+ADD CONSTRAINT ct_leihgebuehr CHECK (gebuehren_leihfrist >= 0 
+									AND	((gebuehren_leihfrist - gebuehren_leihfrist) = 0))
+
+-- max. 2 Verlaengerungen
+ALTER TABLE Ausgeliehene_Exemplare
+ADD CONSTRAINT ct_anzahlmax CHECK (anzahl_verlaengerungen <= 2)
+
